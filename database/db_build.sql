8ac1723b3452ed9b4a916e4140feebac56affb77
nicholaspeal <nicholaspeal@gmail.com>
2017-12-19 15:53:43 +0200

    adding-giffy-to-database Reference issue #8

--- database/db_build.sql
+++ database/db_build.sql
@@ -22,7 +22,16 @@ CREATE TABLE comments (
 );
 
 INSERT INTO pictures (url, title)
-VALUES ('hello', 'test the title');
-
+VALUES ('https://media1.giphy.com/media/Yjc9l1Q6Al1DO/giphy.gif', 'broccoli'),
+('https://media0.giphy.com/media/qpP7VsPNeBtpC/giphy.gif','fail'),
+('https://media2.giphy.com/media/DCy2dnXPeGsO4/giphy.gif', 'sleep'),
+('https://media1.giphy.com/media/l378p0VvTts3st2RG/giphy.gif', 'ears'),
+('https://media0.giphy.com/media/WLbtNNR5TKJBS/giphy.gif', 'yummy'),
+('https://media2.giphy.com/media/mCRJDo24UvJMA/giphy.gif', 'coding'),
+('https://media0.giphy.com/media/nrN8fUJ4EZn5m/giphy.gif','shower'),
+('https://media1.giphy.com/media/S2S0ZDytY6yDm/giphy.gif','wtf'),
+('https://media0.giphy.com/media/DvyLQztQwmyAM/giphy.gif','confused'),
+('https://media2.giphy.com/media/aBfSGPDRDSdqw/giphy.gif','driving'),
+('https://media3.giphy.com/media/RQSuZfuylVNAY/giphy.gif','superdog');
 
 COMMIT;
